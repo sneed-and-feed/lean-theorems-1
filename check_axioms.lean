8c7cd3adb594fed331a1bf4a04e0f860a56107a8
Cthulhu Skates <cthulhu.skates@gmail.com>
2026-08-27 09:05:58 -0700

    chore: clean up obsolete scratch scripts and update .gitignore

--- check_axioms.lean
+++
@@ -1,9 +0,0 @@
-import Formalization.CombinatorialMap.EulerFormula
-
-open CombinatorialMap
-
-#print axioms euler_polyhedron_formula
-#print axioms planar_edge_bound
-#print axioms planar_edge_bound_triangle_free
-#print axioms non_planarity_k5
-#print axioms non_planarity_k33
--- test_tetra.lean
+++
@@ -1,7 +0,0 @@
-import Formalization.CombinatorialMap.Basic
-import Formalization.CombinatorialMap.EulerFormula
-
-open CombinatorialMap
-
-#check tetrahedron
-#eval tetrahedron.edgeCount
